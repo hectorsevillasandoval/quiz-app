916e4b7536ce04535ffd1219e5ba2018be51021a
Hector Sevilla <hse@whitesharkmedia.com>
2016-09-08 22:59:07 -0600

    Adding Vuejs and Materialize

---
+++ app.js
@@ -0,0 +1,36 @@
+var vm = new Vue({
+	el: 'body',
+	data: {
+		questions: [
+			{
+				question: 'Has cocinado bacon en el microondas?',
+				picture_url: 'https://bigoven-res.cloudinary.com/image/upload/t_recipe-256/pork-medallions-wrapped-in-bacon.jpg'
+			},
+			{
+				question: 'Crees que bacon y dulce hacen una buena combinación?',
+				picture_url: ''
+			},
+			{
+				question: 'Hasta la comida mas corriente la he mejorado con Bacon?',
+				picture_url: ''
+			},
+			{
+				question: 'He comido Bacon enrollado en dátiles y esparragos?',
+				picture_url: ''
+			},
+			{
+				question: 'La mejor hora para comer bacon es en la mañana acompañado de huevos a tu gusto. ?',
+				picture_url: ''
+			},
+			{
+				question: 'Mi forma favorita de cocinar Bacon es:',
+				picture_url: ''
+			},
+			{
+				question: 'El bacon se aprovecha mejor cuando se usa para?',
+				picture_url: ''
+			}
+
+		]
+	}
+});
